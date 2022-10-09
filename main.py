9f94658baa29fc3a32eb2ef079fed23893c68fa7
Semyon Kondratev <execute.ink@gmail.com>
2022-10-09 22:47:59 +0200

    Added data uploading and preprocessing via pandas

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 
 import argparse
+import pandas as pd
+
 
 
 def main(filename):
-    print(filename)
-
-
+    with open(filename, 'r') as f:
+        data = pd.read_csv(f, sep='\t', header=None, skiprows=2)
+    print(data)
+    print(data.iloc[0])
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Function for finding peaks of XDR function')
     parser.add_argument('filename')
