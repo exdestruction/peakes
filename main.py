7dca512c4ed2c80b12bc20942913d1bfb82ae5a3
Semyon Kondratev <execute.ink@gmail.com>
2022-10-10 18:24:17 +0200

    Added Diagramms for peaks

--- main.py
+++ main.py
@@ -3,8 +3,8 @@
 import argparse
 import pandas as pd
 import scipy
-
-
+import matplotlib.pyplot as plt
+import numpy as np  
 
 def main(args):
     filename = args.filename
@@ -12,11 +12,24 @@ def main(args):
     height = args.height
     threshold = args.threshold
     distance = args.distance
+    # get data from file provided through command line 
     with open(filename, 'r') as f:
         data = pd.read_csv(f, sep='\t', header=None, skiprows=2)
+        x = data.iloc[:,0]
+        y = data.iloc[:,1]
+        y_log = np.log10(y) 
+
     if method == 'slow_2t-t':
-        peaks = scipy.signal.find_peaks(data.iloc[:,1], height=height, threshold=threshold, distance=distance)
-       
+        peaks = scipy.signal.find_peaks(y_log, height=height, threshold=threshold, distance=distance)
+        idxs = peaks[0]
+        peaks_x = data.iloc[idxs, 0]
+        peaks_y_log = np.log10(data.iloc[idxs, 1])
+
+    fig, ax = plt.subplots()
+    ax.plot(x, y_log)
+    ax.scatter(peaks_x, peaks_y_log, color='r')
+    plt.show()
+
 
 
 if __name__ == '__main__':
