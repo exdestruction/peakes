94221bd4bb1242063870e317c86c3ad147e46739
Semyon Kondratev <execute.ink@gmail.com>
2022-10-19 00:05:21 +0200

    Found peaks for every region of interest

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 import argparse
+import itertools
+
 import pandas as pd
 import scipy
 import matplotlib.pyplot as plt
@@ -66,6 +68,7 @@ def main(args):
     threshold = args.threshold
     distance = args.distance
     prominence = args.prominence
+    number_of_peaks = args.number_of_peaks
 
     # get data from file provided through command line 
     with open(filename, 'r') as f:
@@ -80,11 +83,48 @@ def main(args):
     # applying baseline correction "asymmetric least square method"
     y_res = als(y_log.to_numpy()) 
 
+    # peaks of preprocessed data
+    area_of_interest_peak = scipy.signal.find_peaks(y_res, prominence=prominence)
+    area_of_interest_peak_idxs = area_of_interest_peak[0]
 
-    peaks = scipy.signal.find_peaks(y_res, prominence=prominence)
-    idxs = peaks[0]
-    peaks_x = data.iloc[idxs, 0]
-    peaks_y_log = y_res[idxs]
+    indexes_peaks_overall = []
+    number_of_points_around = 150
+    single_area_peaks = [] 
+    for idx in area_of_interest_peak_idxs:
+        area_around_peak = y_log.iloc[idx - number_of_points_around:idx + number_of_points_around]
+        most_sig_peaks = scipy.signal.find_peaks(area_around_peak)
+        most_sig_peaks_idx = most_sig_peaks[0]
+        peaks_data = area_around_peak.iloc[most_sig_peaks_idx]
+        prominence = scipy.signal.peak_prominences(area_around_peak, most_sig_peaks[0])
+
+        # pandas.Series to pandas.DataFrame to be able to acces indexes
+        peaks_data = peaks_data.to_frame()
+        peaks_data['prominence'] = prominence[0]
+
+        # number_of_peaks of most significant peaks
+        peaks_data_sorted = peaks_data.sort_values('prominence')
+        peaks_single_area = peaks_data_sorted.tail(number_of_peaks)
+
+        idxs_peaks_single_area = np.sort(peaks_single_area.index.to_numpy())
+        # single_area['start_idx'] = area_around_peak.index.to_numpy()[0]
+        single_area_peaks = idxs_peaks_single_area.tolist()
+        indexes_peaks_overall += single_area_peaks
+        # print(peaks_single_area.loc[idxs_peaks_single_area]
+        # single_area = {}
+        # print(idxs_peaks_single_area)
+    # raise
+    # peaks_x = data.iloc[idxs, 0]
+    # peaks_idxs = [elem for elem in indexes_peaks_overall]
+    # print(peaks_idxs)
+    # # raise
+    # # flatten list of lists
+    # peaks_idxs = list(itertools.chain.from_iterable(peaks_idxs))
+    # print(peaks_idxs)
+
+    # raise
+    peaks_x = data.loc[indexes_peaks_overall, 0]
+    # print(peaks_x)
+    peaks_y_log = y_log.loc[indexes_peaks_overall]
     alphas = peaks_x /180 * np.pi
 
     if method == 'slow_2t-t':
@@ -112,6 +152,7 @@ if __name__ == '__main__':
     parser.add_argument("-t", '--threshold', default=.1)
     parser.add_argument("-d", '--distance', default=100)
     parser.add_argument("-p", "--prominence",type=int, default=0.5)
+    parser.add_argument("-np", "--number_of_peaks", type=int, default=3)
     args = parser.parse_args()
     main(args)
 
